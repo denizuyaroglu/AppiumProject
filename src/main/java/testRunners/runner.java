b7d050a629801f9d7b524726677c789b5f6fdd25
Deniz.Uyaroglu <denizuyaroglu@gmail.com>
2023-01-05 09:15:28 +0300

    Appium projesi

--- src/main/java/testRunners/runner.java
+++ src/main/java/testRunners/runner.java
@@ -1,4 +1,20 @@
 package testRunners;
 
-public class runner {
+
+import io.appium.java_client.AppiumDriver;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import util.DriverFactory;
+
+@CucumberOptions(
+        features = {"src/test/java/features"},
+        glue = {"StepDefinitions","util"},
+        tags = "",
+        plugin = {
+                "summary","pretty","html:Reports/CucumberReport/Reports.html"
+        }
+)
+
+public class runner extends AbstractTestNGCucumberTests {
+    static AppiumDriver driver = DriverFactory.getDriver();
 }
